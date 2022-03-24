6ddbe23c618cae956755e3d934f298d09cf885fa
Mariano Banchio <mbweb@LIVE.COM.AR>
2022-03-23 22:00:25 -0300

    fix

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -3,6 +3,6 @@ import axios from 'axios'
 export default axios.create({
     baseURL: 'https://api.yelp.com/v3/businesses',
     headers: {
-        Authorization: "Bearer OJdQuatFiP-PvKJZfX8RoTS8DwWjDqkY-FYbDTZFByR6zvr4Lx2m--T7q8DpkVqyyGeeXAa9S-VUeAmOAei-7w2FwipfxduzAJnqOC9Vr0kiD-klmFYhWaEgaUY6YnYx"
+        Authorization: "Bearer "
     }
 })
